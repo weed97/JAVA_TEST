00870acd9222e96b30a265a6e0e9267541a8e976
weed97 <weed9935@gmail.com>
2024-06-13 21:16:25 +0900

    OverMemberException 클래스

---
+++ src/main/java/com/ohgiraffers/member/controller/OverMemberException.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.member.controller;
+
+public class OverMemberException extends RuntimeException {
+    public OverMemberException(String message) {
+        super(message);
+    }
+}
